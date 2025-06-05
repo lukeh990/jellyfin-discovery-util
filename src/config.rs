18ea4d32328affe3245c0b1cba67fd56cf90fe02
Luke Harding <luke@lukeh990.io>
2025-06-04 21:47:55 -0400

    1.0.0

--- src/config.rs
+++ src/config.rs
@@ -5,8 +5,100 @@
  * See the file "LICENSE" in the root of this project.
  */
 
-struct Config {}
+use log::{debug, trace, warn};
+use serde::{Deserialize, Serialize};
+use std::{
+    env, fs, io,
+    path::{self, PathBuf},
+};
+use thiserror::Error;
 
-fn read_config() {}
+#[derive(Serialize, Deserialize)]
+pub struct ServerConfig {
+    pub url: String,
+    pub id: String,
+    pub name: String,
+}
 
-fn create_sample_config() {}
+impl ServerConfig {
+    pub fn new(url: &str, id: &str, name: &str) -> ServerConfig {
+        ServerConfig {
+            url: url.to_string(),
+            id: id.to_string(),
+            name: name.to_string(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    version: String,
+    pub port: u16,
+    pub server: Vec<ServerConfig>,
+}
+
+impl Config {
+    pub fn new(port: u16, servers: Vec<ServerConfig>) -> Config {
+        Config {
+            version: "1".to_string(),
+            port,
+            server: servers,
+        }
+    }
+}
+
+pub fn read_config() -> Result<Config, ConfigError> {
+    trace!("Config File Version: v1");
+
+    let file_path = match env::var("JDU_CONF") {
+        Ok(val) => path::absolute(&val)?,
+        Err(_) => {
+            debug!("Using default config file name.");
+            path::absolute("discover.toml")?
+        }
+    };
+
+    if file_path.exists() {
+        trace!("Using existing file");
+
+        let contents = &fs::read_to_string(file_path)?;
+
+        let config: Config = toml::from_str(contents)?;
+
+        Ok(config)
+    } else {
+        warn!(
+            "Creating Sample Config! It can be found at: {}",
+            file_path.display()
+        );
+
+        create_sample_config(&file_path)
+    }
+}
+
+fn create_sample_config(file_path: &PathBuf) -> Result<Config, ConfigError> {
+    let config = Config::new(
+        7359,
+        vec![ServerConfig::new(
+            "http://jellyfin-test.local",
+            "CHANGEME",
+            "Test Jellyfin Server",
+        )],
+    );
+
+    let config_str: String = toml::to_string(&config)?;
+
+    fs::write(file_path, config_str)?;
+
+    Ok(config)
+}
+
+#[derive(Error, Debug)]
+pub enum ConfigError {
+    #[error("Error While Deserializing Struct")]
+    Derserialize(#[from] toml::de::Error),
+    #[error("Error While Serializing Struct")]
+    Serialize(#[from] toml::ser::Error),
+    #[error("Error While Preforming IO Operation")]
+    Io(#[from] io::Error),
+}
--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,93 @@
 
 mod config;
 
-use log::info;
-use std::error;
+use bytes::{Bytes, BytesMut};
+use config::ServerConfig;
+use log::{debug, error, info, trace};
+use serde::Serialize;
+use std::{
+    error,
+    net::{SocketAddr, UdpSocket},
+    process,
+};
 use systemd_journal_logger::{JournalLog, connected_to_journal};
 
 fn main() {
     enable_logging();
 
-    info!("Starting Jellyfin Discovery Utility.")
+    info!("Starting Jellyfin Discovery Utility. v1.0.0");
+
+    let config = handle_error(config::read_config(), 100);
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], config.port));
+    let socket = handle_error(UdpSocket::bind(addr), 101);
+
+    debug!("Created socket and bound to 0.0.0.0:{}", config.port);
+
+    let preconstructed_response = preconstruct_response(config.server);
+    trace!(
+        "Pre-generated resonse complete. {} servers loaded",
+        preconstructed_response.len()
+    );
+
+    loop {
+        let mut buf = BytesMut::zeroed(22);
+        let (amt, src) = handle_error(socket.recv_from(&mut buf), 102);
+
+        trace!("Injested {} bytes from {}", amt, &src);
+
+        if &buf[..] == b"Who is JellyfinServer?" {
+            trace!("Valid Discovery");
+
+            for response in &preconstructed_response {
+                handle_error(socket.send_to(response, src), 104);
+                trace!("Sent response");
+            }
+        } else {
+            trace!("Invalid Discovery");
+        }
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Response {
+    address: String,
+    id: String,
+    name: String,
+    endpoint_address: Option<()>,
+}
+
+fn preconstruct_response(server_config: Vec<ServerConfig>) -> Vec<Bytes> {
+    let mut output = vec![];
+
+    for config in server_config {
+        let response = Response {
+            address: config.url,
+            id: config.id,
+            name: config.name,
+            endpoint_address: None,
+        };
+
+        let response_vec = handle_error(serde_json::to_vec(&response), 103);
+        output.push(Bytes::from(response_vec));
+    }
+
+    output
+}
+
+fn handle_error<X, Y>(res: Result<X, Y>, code: i32) -> X
+where
+    Y: error::Error,
+{
+    match res {
+        Ok(x) => x,
+        Err(e) => {
+            error!("{}", dbg!(e));
+
+            process::exit(code);
+        }
+    }
 }
 
 fn enable_logging() {
